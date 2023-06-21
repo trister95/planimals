ab60446bf05fa9246026761d221a47a9629b9773
trister95 <63931136+trister95@users.noreply.github.com>
2023-06-21 12:08:13 +0200

    Update metrics.py

--- train_model/metrics.py
+++ train_model/metrics.py
@@ -26,3 +26,28 @@ def compute_metrics(p,id2tag):
         "f1": results["overall_f1"],
         "accuracy": results["overall_accuracy"],
     }
+
+def compute_metrics2(p):
+    predictions, labels = p
+    predictions = np.argmax(predictions, axis=2)
+
+    unique_tags = ("anything_else", "animal")
+    tag2id = {tag: id for id, tag in enumerate(unique_tags)}
+    id2tag = {id: tag for tag, id in tag2id.items()}
+
+    true_predictions = [
+        [id2tag[p] for (p, l) in zip(prediction, label) if l != -100]
+        for prediction, label in zip(predictions, labels)
+    ]
+    true_labels = [
+        [id2tag[l] for (p, l) in zip(prediction, label) if l != -100]
+        for prediction, label in zip(predictions, labels)
+    ]
+
+    results = seqeval.compute(predictions=true_predictions, references=true_labels)
+    return {
+        "precision": results["overall_precision"],
+        "recall": results["overall_recall"],
+        "f1": results["overall_f1"],
+        "accuracy": results["overall_accuracy"],
+    }

de57fd303206f869c375165c44829974a587f9d1
trister95 <63931136+trister95@users.noreply.github.com>
2023-06-27 15:03:54 +0200

    Update metrics.py

--- train_model/metrics.py
+++ train_model/metrics.py
@@ -1,33 +1,7 @@
 import numpy as np
 import evaluate
 
-def compute_metrics(p,id2tag):
-    """ 
-    Define metrics.
-    """
-    seqeval = evaluate.load("seqeval")
-
-    predictions, labels = p
-    predictions = np.argmax(predictions, axis=2)
-
-    true_predictions = [
-        [id2tag[p] for (p, l) in zip(prediction, label) if l != -100]
-        for prediction, label in zip(predictions, labels)
-    ]
-    true_labels = [
-        [id2tag[l] for (p, l) in zip(prediction, label) if l != -100]
-        for prediction, label in zip(predictions, labels)
-    ]
-
-    results = seqeval.compute(predictions=true_predictions, references=true_labels)
-    return {
-        "precision": results["overall_precision"],
-        "recall": results["overall_recall"],
-        "f1": results["overall_f1"],
-        "accuracy": results["overall_accuracy"],
-    }
-
-def compute_metrics2(p):
+def compute_metrics(p):
     seqeval = evaluate.load("seqeval")
 
     predictions, labels = p

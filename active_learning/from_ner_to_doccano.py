9a8aa61a5066ddba15b189727bccb8b73081e88d
trister95 <63931136+trister95@users.noreply.github.com>
2023-08-02 16:30:35 +0200

    Update from_ner_to_doccano.py

--- active_learning/from_ner_to_doccano.py
+++ active_learning/from_ner_to_doccano.py
@@ -1,12 +1,11 @@
-def hf_output_for_doccano(sentences, classifier):
+import json
+from tqdm import tqdm
+def to_doccano_format(sentences, classifier):
   json_lst = []
-  for sentence in sentences:
+  for sentence in tqdm(sentences):
     hf_output = classifier(sentence)
-    labels = []
-    for sentence_part in hf_output:
-      if sentence_part["entity_group"] == "animals":
-        label = [sentence_part["start"], sentence_part["end"], "ANIMAL"]
-        labels.append(label)
-    data = {"text":sentence, "labels":labels}
-    json_lst.append(data)
+    if bool(hf_output) == True:
+      labels = [[d["start"], d["end"], d["entity_group"]] for d in hf_output]
+      data ={"text":sentence, "labels":labels}
+      json_lst.append(json.dumps(data))
   return json_lst

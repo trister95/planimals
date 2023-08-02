55b50ff73d3eb868b59b090910a783210066df04
trister95 <63931136+trister95@users.noreply.github.com>
2023-08-02 10:15:03 +0200

    fix indentation

--- utils/get_lst_from_csv.py
+++ utils/get_lst_from_csv.py
@@ -6,7 +6,7 @@ def get_lst_from_csv(value):
   since I expected that the first value declaratiod
   wouldn't be needed (but it turns out it is).
   """
-    value = ast.literal_eval(value)
-    value_str = value.decode('utf-8')
-    return ast.literal_eval(value_str)
+  value = ast.literal_eval(value)
+  value_str = value.decode('utf-8')
+  return ast.literal_eval(value_str)
 

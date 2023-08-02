d341fa06cab27e356cd1bee9d6a0be41cd0368ef
trister95 <63931136+trister95@users.noreply.github.com>
2023-08-02 09:54:29 +0200

    Update tag_sentence.py
    function for tagging has OIB structure now and is easier to use for other case. Langdetection transferred to utils.

--- preprocess/tag_sentence.py
+++ preprocess/tag_sentence.py
@@ -1,33 +1,31 @@
-from langdetect import detect
-def check_for_organisms(sentence_lst, organism_lst):
+def check_for_entities(sentence_lst, entity_lst, start_numbering):
     """
-    This functions tags organisms names ngrams up
+    This functions tags entities names ngrams up
     to a lenght of 3 parts. Check if there aren't
     longer names in the reference_lst.
     """
+    sentence_lst = [word.lower() for word in sentence_lst]
     tag_lst = []
     i = 0
     while i < len(sentence_lst):
         word = sentence_lst[i]
         if i < len(sentence_lst) - 2:
             phrase = word + " " + sentence_lst[i+1] + " " + sentence_lst[i+2]
-            if phrase in organism_lst:
-                tag_lst.extend([1, 1, 1])
+            if phrase in entity_lst:
+                tag_lst.extend([start_numbering, start_numbering+1,start_numbering+1])
                 i += 3
                 continue
         if i < len(sentence_lst) - 1:
             phrase = word + " " + sentence_lst[i+1]
-            if phrase in organism_lst:
-                tag_lst.extend([1, 1])
+            if phrase in entity_lst:
+                tag_lst.extend([start_numbering, start_numbering+1])
                 i += 2
                 continue
-        if word in organism_lst:
-            tag_lst.append(1)
+        if word in entity_lst:
+            tag_lst.append(start_numbering)
         else:
             tag_lst.append(0)
         i += 1
     return tag_lst
-    
-def find_language(sentence_lst):
-    return detect((" ".join(sentence_lst)))
+
 

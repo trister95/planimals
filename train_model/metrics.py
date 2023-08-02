f1b4b3e600563949cf2723f6ce83ac2f8099f43d
trister95 <63931136+trister95@users.noreply.github.com>
2023-08-02 10:57:57 +0200

    Update metrics.py

--- train_model/metrics.py
+++ train_model/metrics.py
@@ -1,17 +1,21 @@
 import numpy as np
 import evaluate
 seqeval = evaluate.load("seqeval")
+"""
+Thing wants to have a label_lst somewhere. 
+Not fully figured out how. 
+"""
 
-def compute_metrics_for_initial_training(p, tag2id, id2tag):
+def compute_metrics_for_initial_training(p):
     predictions, labels = p
     predictions = np.argmax(predictions, axis=2)
 
     true_predictions = [
-        [id2tag[p] for (p, l) in zip(prediction, label) if l != -100]
+        [label_lst[p] for (p, l) in zip(prediction, label) if l != -100]
         for prediction, label in zip(predictions, labels)
     ]
     true_labels = [
-        [id2tag[l] for (p, l) in zip(prediction, label) if l != -100]
+        [label_lst[l] for (p, l) in zip(prediction, label) if l != -100]
         for prediction, label in zip(predictions, labels)
     ]
 

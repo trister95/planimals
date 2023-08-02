3d855a4c4ad64b5af60c57b4784fffa66f5df089
trister95 <63931136+trister95@users.noreply.github.com>
2023-08-02 09:58:22 +0200

    Create get_lst_from_csv.py

---
+++ get_lst_from_csv.py
@@ -0,0 +1,12 @@
+import ast
+
+def get_lst_from_csv(value):
+  """
+  Would be smart to do some more checking,
+  since I expected that the first value declaratiod
+  wouldn't be needed (but it turns out it is).
+  """
+    value = ast.literal_eval(value)
+    value_str = value.decode('utf-8')
+    return ast.literal_eval(value_str)
+

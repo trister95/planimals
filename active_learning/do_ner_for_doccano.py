02077002d7b8c6b94798b62cdeba42dbea23892c
trister95 <63931136+trister95@users.noreply.github.com>
2023-08-09 11:15:00 +0200

    Update do_ner_for_doccano.py

--- active_learning/do_ner_for_doccano.py
+++ active_learning/do_ner_for_doccano.py
@@ -6,6 +6,6 @@ def to_doccano_format(sentences, classifier):
     hf_output = classifier(sentence)
     if bool(hf_output) == True:
       labels = [[d["start"], d["end"], d["entity_group"]] for d in hf_output]
-      data ={"text":sentence, "labels":labels}
+      data ={"text":sentence, "label":labels}
       json_lst.append(json.dumps(data))
   return json_lst

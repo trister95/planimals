0e5779e734a3d0983395d6d2b63fa806680c1725
trister95 <63931136+trister95@users.noreply.github.com>
2023-08-15 14:44:09 +0200

    Update and rename from_doccano_to_ner.py to from_doccano_to_ner_DEPR.py

--- active_learning/from_doccano_to_ner_DEPR.py
+++ active_learning/from_doccano_to_ner_DEPR.py
@@ -1,37 +1,6 @@
 import ucto
 import json
 
-
-
-import spacy
-from spacy.training import offsets_to_biluo_tags, biluo_to_iob
-nlp = spacy.blank("nl")
-
-
-def jsonl_to_ner_format(filepath, tag2id, text_col="text", label_col="label"):
-  lst = []
-  with open(filepath, 'r') as file:
-    for line in file:
-      data = json.loads(line)
-      text, entities = data[text_col], data[label_col]
-      doc = nlp(text)
-      biluo_tags = offsets_to_biluo_tags(doc, entities)
-      iob_tags = biluo_to_iob(biluo_tags)
-      try:
-        number_tags = [tag2id[iob] for iob in iob_tags]
-        together = [[str(word) for word in doc],number_tags]
-        lst.append(together)
-      except:
-        print(doc)
-  return lst
-
-#evrything below is probably not needed anymore
-
-
-
-
-
-
 configurationfile_ucto = "tokconfig-nld-historical"
 tokenizer = ucto.Tokenizer(configurationfile_ucto)
 

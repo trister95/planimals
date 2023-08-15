ad991a6193820781a66548a9da4c8d53333450a0
trister95 <63931136+trister95@users.noreply.github.com>
2023-08-15 14:48:38 +0200

    Update metrics.py

--- train_model/metrics.py
+++ train_model/metrics.py
@@ -26,3 +26,28 @@ def compute_metrics_for_initial_training(p, label_lst):
         "f1": results["overall_f1"],
         "accuracy": results["overall_accuracy"]
     }
+
+def compute_metrics_without_O(p, label_lst):
+    predictions, labels = p
+    predictions = np.argmax(predictions, axis=2)
+
+    # Determine the index of the "O" label
+    O_index = label_lst.index("O")
+
+    # Filter out predictions and labels with -100 and "O" labels
+    true_predictions = [
+        [label_lst[p] for (p, l) in zip(prediction, label) if l != -100 and p != O_index and l != O_index]
+        for prediction, label in zip(predictions, labels)
+    ]
+    true_labels = [
+        [label_lst[l] for (p, l) in zip(prediction, label) if l != -100 and p != O_index and l != O_index]
+        for prediction, label in zip(predictions, labels)
+    ]
+
+    results = seqeval.compute(predictions=true_predictions, references=true_labels, zero_division=0)
+    return {
+        "precision": results["overall_precision"],
+        "recall": results["overall_recall"],
+        "f1": results["overall_f1"],
+        "accuracy": results["overall_accuracy"]
+    }
